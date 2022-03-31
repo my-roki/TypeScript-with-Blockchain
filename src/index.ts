c803cbb8fc736b17a948dbb7094f0c4bd3fd89e0
my-roki <anthonychangrok@gmail.com>
2022-03-31 14:27:22 +0900

    update tsc-watch mode

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "roki",
+  age = 29,
+  gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, ang you are ${gender}`;
+};
+
+console.log(sayHi(name, age, gender));
+
+export {};
