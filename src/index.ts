2f253d8ca13d7fe9486d84a65a584268a768085a
my-roki <anthonychangrok@gmail.com>
2022-03-31 14:51:41 +0900

    learn interface and class in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-const name = "roki",
-  age = 29,
-  gender = "male";
+// interface
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, ang you are ${gender}`;
+const person = {
+  name: "roki",
+  age: 29,
+  gender: "male",
 };
 
-console.log(sayHi(name, age, gender));
+// class
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const toki = new Human("toki", 28, "femle");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, ang you are ${person.gender}`;
+};
+
+console.log(sayHi(person));
+console.log(sayHi(toki));
 
 export {};
