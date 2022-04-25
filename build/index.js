84faa52dd94bf46aa7fe0505f57cf70fd734c443
my-roki <anthonychangrok@gmail.com>
2022-04-25 19:29:36 +0900

    TypeScript Blockchain Update 2022 version. learn dev infra of type script

---
+++ build/index.js
@@ -0,0 +1,10 @@
+var Block = /** @class */ (function () {
+    function Block(data) {
+        this.data = data;
+    }
+    Block.hello = function () {
+        return "hello type script!";
+    };
+    return Block;
+}());
+var hello = function () { return "hi"; };
---
+++ src/myPackage.js
@@ -0,0 +1,18 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @returns boolean
+ */
+export function init(config) {
+  return true;
+}
+
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns number
+ */
+export function exit(code) {
+  return code + 1;
+}
